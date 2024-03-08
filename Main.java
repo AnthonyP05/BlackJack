9fb0c4df167cfef4bc28ba6dd8dbba6fd862a159
Anthony <49537170+AnthonyP05@users.noreply.github.com>
2024-03-08 01:13:28 -0500

    Added more TODO's for later

--- Main.java
+++ Main.java
@@ -1,14 +1,24 @@
 public class Main {
     public static void main(String[] args) {
+        // TODO: Add a banking system. The banking
+        //  system is supposed to record the amount
+        //  of money you have start to end.
 
-        /*
-         * TODO: Add a banking system
-         *       The banking system is supposed to record the amount of money you have start
-         *       to end.
-         */
+        // TODO: Finish If player split, then create a new
+        //  deck with that player's same bet. Empty function
+        //  left in BJPlayer.java -> removeFromHand() to create
+        //  a new deck for the player.
 
-        // TODO: Finish If player split, then create a new deck with that player's same bet
+        // TODO: Fix if player busts then they get no more turns.
 
+        // TODO: If both the player and the dealer
+        //  decide not to hit anymore (i.e., they
+        //  both choose to stand), the game should
+        //  determine the winner based on their
+        //  current scores. The player with the score
+        //  closest to 21 without exceeding it wins.
+
+        // NOTE: Left line 89 of BJGame.java in case i need to revert changes.
 
         BJGame game = new BJGame();
         game.startGame();
